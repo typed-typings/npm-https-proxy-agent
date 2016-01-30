6e0cb6d6cfdeddffb06377c539790b9c501bce9b
Blake Embrey <hello@blakeembrey.com>
2016-01-30 12:06:05 -0800

    Make URL properties optional in constructor

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { Agent } from 'http';
 
 declare class HttpsProxyAgent extends Agent {
-  constructor (uri: string | { protocol: string; host: string; port?: string });
+  constructor (uri: string | { protocol?: string; host?: string; hostname?: string; port?: string });
 }
 
 export = HttpsProxyAgent;
